475e4ecb912846e6592e39385ef4bcb28caba591
Alex Clink <code@alexclink.com>
2015-02-14 16:09:40 -0600

    updated to version 1.0.2

--- vhost.rb
+++ vhost.rb
@@ -1,17 +1,17 @@
 class Vhost < Formula
   homepage "https://github.com/SleepingInsomniac/vhost"
-  url "https://github.com/SleepingInsomniac/vhost/archive/1.0.1.tar.gz"
-  sha1 "71dd6cb5107843beefc89b24a9cb51bf9d4658bb"
-  
+  url "https://github.com/SleepingInsomniac/vhost/archive/1.0.2.tar.gz"
+  sha1 "77b2de8d408b40b2304cf9244c6e7de55b30074c"
+
   def install
     bin.install "vhost"
     etc.install "vhosts-conf" unless File.exists? File.join(etc, "vhosts-conf")
   end
-  
+
   def caveats
     "Recommended: dnsmasq (a light weight dns server)"
   end
-  
+
   test do
     system "#{bin}/vhost"
   end
