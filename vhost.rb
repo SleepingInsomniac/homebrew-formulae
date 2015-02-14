baffc91b960f1bee83dd9d3f698b3b20909cf502
Alex Clink <code@alexclink.com>
2015-02-14 16:53:21 -0600

    version 1.0.1

--- vhost.rb
+++ vhost.rb
@@ -1,7 +1,7 @@
 class Vhost < Formula
   homepage "https://github.com/SleepingInsomniac/vhost"
-  url "https://github.com/SleepingInsomniac/vhost/archive/1.0.0.tar.gz"
-  sha1 "4062da25c2d6215c0cb9ee502d81fd9881a29171"
+  url "https://github.com/SleepingInsomniac/vhost/archive/1.0.1.tar.gz"
+  sha1 "6d8b6a7e37868b4bb9d5ab77a209482310ff4093"
 
   def install
     bin.install "vhost"
